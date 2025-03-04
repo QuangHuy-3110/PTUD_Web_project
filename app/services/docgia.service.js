98aa90d2ba55e373b15afdbd93a8a4d982ca5c13
QuangHuy-3110 <quanghuy.0511204@gmail.com>
2025-03-04 21:44:36 +0700

    Cap nhat backend

---
+++ app/services/docgia.service.js
@@ -0,0 +1,118 @@
+const { ObjectId } = require('mongodb');
+
+class DocgiaService {
+    constructor(client) {
+        this.Docgia = client.db().collection('docgia');
+    }
+//    dinh ngia cac phuong thuc
+    create_pass (length = 6){
+        let str = "";
+        while (str.length < length) {
+            str += Math.random().toString(36).substring(2);
+        }
+        return str.substring(0, length);
+    }
+
+    create_user (name, id){
+        const nameParts = name.trim().split(' ');
+        const firstNamePart = nameParts[0].toLowerCase();
+        const lastNamePart = nameParts[1] ? nameParts[1].toLowerCase() : ''; 
+        const loginName = firstNamePart + lastNamePart + id;
+        return loginName;
+    }
+
+    extractDocgiaData(payload) {
+        const docgia = {
+            _id: payload._id,
+            tenDG: payload.tenDG,
+            diachiDG: payload.diachiDG,
+            gioitinhDG: payload.gioitinhDG,
+            ngaysinhDG: payload.ngaysinhDG,
+            dienthoaiDG: payload.dienthoaiDG,
+            taikhoanDG: this.create_user (payload.tenDG, payload._id),
+            matkhauDG: this.create_pass(),
+        };
+        // remove undefined fields
+        Object.keys(docgia).forEach(
+            (key) => docgia[key] === undefined && delete docgia[key]
+        );
+        return docgia;
+    }
+
+    async create(payload) {
+        
+        const docgia = this.extractDocgiaData(payload);
+        
+        const result = await this.Docgia.findOneAndUpdate(
+            docgia,
+            { $set: docgia },
+            { returnDocument: 'after', upsert: true }
+            );
+        return result;
+    }
+
+
+    async find (filter) {
+        const cursor = this.Docgia.find(filter);
+        return await cursor.toArray();
+    }
+
+    async findByName(name) {
+        return await this.find({
+            tenDG: {$regex: new RegExp(new RegExp(name)), $options: 'i'}
+        });
+    }
+
+    async findByTaiKhoan(taikhoanDG) {
+        return await this.find({
+            taikhoanDG: taikhoanDG
+        });
+    }
+
+    async findById(id) {
+        // if (!ObjectId.isValid(id)) {
+        //     throw new Error('Invalid ObjectId');
+        // }
+        
+        try {
+            const docgia = await this.Docgia.findOne({
+                _id: id,
+            });
+            if (!docgia) {
+                console.error('Không tìm thấy tài liệu độc giả:', id);
+                throw new Error('Không tìm thấy tài liệu độc giả với id');
+            }
+            return docgia;
+        } catch (error) {
+            console.error('Có lỗi xảy ra khi tìm kiếm độc giả:', error);
+            throw error;
+        }
+    }
+
+    async update(id, payload) {
+        const filter = {
+            _id: id ? id : null,
+        };    
+        const update = this.extractDocgiaData(payload);
+        const result = await this.Docgia.findOneAndUpdate(
+            filter,
+            { $set: update },
+            { returnDocument: 'after' }
+        );
+        return result.updatedExisting;
+    }
+
+    async delete(id) {
+        const result = await this.Docgia.findOneAndDelete({
+            _id: id ? id : null,
+        });
+        return result;
+    }
+
+    async deleteAll (){
+        const result = await this.Docgia.deleteMany({});
+        return result.deletedCount;
+    }
+}
+
+module.exports = DocgiaService;
---
+++ app/services/nhanvien.service.js
@@ -0,0 +1,117 @@
+const { ObjectId } = require('mongodb');
+
+class NhanvienService {
+    constructor(client) {
+        this.Nhanvien = client.db().collection('nhanvien');
+    }
+//    dinh ngia cac phuong thuc
+    create_pass (length = 6){
+        let str = "";
+        while (str.length < length) {
+            str += Math.random().toString(36).substring(2);
+        }
+        return str.substring(0, length);
+    }
+
+    create_user (name, id){
+        const nameParts = name.trim().split(' ');
+        const firstNamePart = nameParts[0].toLowerCase();
+        const lastNamePart = nameParts[1] ? nameParts[1].toLowerCase() : ''; 
+        const loginName = firstNamePart + lastNamePart + id;
+        return loginName;
+    }
+
+    extractNhanvienData(payload) {
+        const nhanvien = {
+            _id: payload._id,
+            tenNV: payload.tenNV,
+            diachiNV: payload.diachiNV,
+            chucvuNV: payload.chucvuNV,
+            dienthoaiNV: payload.dienthoaiNV,
+            taikhoanNV: this.create_user (payload.tenNV, payload._id),
+            matkhauNV: this.create_pass(),
+        };
+        // remove undefined fields
+        Object.keys(nhanvien).forEach(
+            (key) => nhanvien[key] === undefined && delete nhanvien[key]
+        );
+        return nhanvien;
+    }
+
+    async create(payload) {
+        
+        const nhanvien = this.extractNhanvienData(payload);
+        
+        const result = await this.Nhanvien.findOneAndUpdate(
+            nhanvien,
+            { $set: nhanvien },
+            { returnDocument: 'after', upsert: true }
+            );
+        return result;
+    }
+
+
+    async find (filter) {
+        const cursor = this.Nhanvien.find(filter);
+        return await cursor.toArray();
+    }
+
+    async findByName(name) {
+        return await this.find({
+            tenNV: {$regex: new RegExp(new RegExp(name)), $options: 'i'}
+        });
+    }
+
+    async findByTaiKhoan(name) {
+        return await this.find({
+            taikhoanNV: name
+        });
+    }
+
+    async findById(id) {
+        // if (!ObjectId.isValid(id)) {
+        //     throw new Error('Invalid ObjectId');
+        // }
+        
+        try {
+            const nhanvien = await this.Nhanvien.findOne({
+                _id: id,
+            });
+            if (!nhanvien) {
+                console.error('Không tìm thấy tâì liệu nhân viên:', id);
+                throw new Error('Không tìm thấy tâì liệu nhân viên với id');
+            }
+            return nhanvien;
+        } catch (error) {
+            console.error('Có lỗi xảy ra trong khi tìm kiếm nhân viên:', error);
+            throw error;
+        }
+    }
+
+    async update(id, payload) {
+        const filter = {
+            _id: id ? id : null,
+        };    
+        const update = this.extractNhanvienData(payload);
+        const result = await this.Nhanvien.findOneAndUpdate(
+            filter,
+            { $set: update },
+            { returnDocument: 'after' }
+        );
+        return result.updatedExisting;
+    }
+
+    async delete(id) {
+        const result = await this.Nhanvien.findOneAndDelete({
+            _id: id ? id : null,
+        });
+        return result;
+    }
+
+    async deleteAll (){
+        const result = await this.Nhanvien.deleteMany({});
+        return result.deletedCount;
+    }
+}
+
+module.exports = NhanvienService;
