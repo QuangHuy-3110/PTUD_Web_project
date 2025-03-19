b8d1090b306ca73b20a755ea3cdbbfaf1d895fe8
QuangHuy-3110 <quanghuy.0511204@gmail.com>
2025-03-20 00:49:14 +0700

    Chỉnh sửa header user

--- library-frontend/src/views/Docgia.vue
+++ library-frontend/src/views/Docgia.vue
@@ -4,12 +4,12 @@
   <!-- Container wrapper -->
   <div class="container">
     <!-- Navbar brand -->
-    <div class="d-flex align-items-center p-3" style=" max-width: 250px; margin-right: 200px;">
+    <a href="#" @click="nav_pick = 0, updateSach" class="d-flex align-items-center p-3" style=" max-width: 250px; margin-right: 200px; text-decoration: none;">
         <img src="https://cdn-icons-png.flaticon.com/512/224/224595.png" width="60" class="me-2">
-        <p class="mb-0 fw-bold fs-2 text-muted" style="font-family: 'Poppins', sans-serif; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);">
+        <p  class="mb-0 fw-bold fs-2 text-muted" style="font-family: 'Poppins', sans-serif; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);">
             Library
         </p>
-    </div>
+    </a>
 
 
     <!-- Search form -->
@@ -26,19 +26,38 @@
     <!-- Collapsible wrapper -->
     <div class="collapse navbar-collapse " id="navbarSupportedContent">
       <!-- Left links -->
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
-        <li class="nav-item" >
-          <a class="nav-link d-flex flex-column text-center active" aria-current="page" href="#" @click="nav_pick = 0, updateSach"><i class="fas fa-home fa-lg my-2"></i><span class="small">Trang chủ</span></a>
-        </li>
-        <li class="nav-item" >
-          <a class="nav-link d-flex flex-column text-center active" aria-current="page" href="#" @click="nav_pick = 1, updateList_y"><i class="fa-solid fa-paper-plane"></i><span class="small">Yêu cầu</span></a>
+      <ul class="navbar-nav d-flex justify-content-evenly w-100 mb-2 mb-lg-0">
+        <li class="nav-item">
+            <a class="nav-link d-flex flex-column align-items-center text-center active" 
+               aria-current="page" href="#" 
+               @click="nav_pick = 0, updateSach">
+                <i class="fa-solid fa-home my-2 fs-5"></i>
+                <span class="small">Trang chủ</span>
+            </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#" @click="nav_pick = 2, updateList_m"><i class="fa-solid fa-book"></i><span class="small">Đang mượn</span></a>
+            <a class="nav-link d-flex flex-column align-items-center text-center active" 
+               aria-current="page" href="#" 
+               @click="nav_pick = 1, updateList_y">
+                <i class="fa-solid fa-paper-plane my-2 fs-5"></i>
+                <span class="small">Yêu cầu</span>
+            </a>
         </li>
-
         <li class="nav-item">
-          <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#" @click="nav_pick = 3, updateList_t"><i class="fa-solid fa-clock"></i><span class="small">Lịch sử</span></a>
+            <a class="nav-link d-flex flex-column align-items-center text-center active" 
+               aria-current="page" href="#" 
+               @click="nav_pick = 2, updateList_m">
+                <i class="fa-solid fa-book my-2 fs-5"></i>
+                <span class="small">Đang mượn</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link d-flex flex-column align-items-center text-center active" 
+               aria-current="page" href="#" 
+               @click="nav_pick = 3, updateList_t">
+                <i class="fa-solid fa-clock my-2 fs-5"></i>
+                <span class="small">Lịch sử</span>
+            </a>
         </li>
         <!-- Dropdown -->
         <li class="nav-item dropdown">
@@ -53,7 +72,7 @@
             <img
               src="https://cdn-icons-png.flaticon.com/512/748/748493.png"
               class="rounded-circle"
-              height="40"
+              height="50"
               alt=""
               loading="lazy"
             />
